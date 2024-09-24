3f7bcd8cd54f9e29c820006e4fb41172d11f544f
djcadaniel <djcadaniel@gmail.com>
2024-09-24 17:03:02 -0500

    add store list

--- src/components/PatientForm.tsx
+++ src/components/PatientForm.tsx
@@ -1,13 +1,15 @@
 import { useForm } from 'react-hook-form';
 import { Error } from './Error';
 import { DraftPatient } from '../types';
+import { usePatientStore } from '../store';
 
 export const  PatientForm =() =>{
 
+  // const {addPatient} = usePatientStore()
+  const addPatient = usePatientStore(state =>state.addPatient)
   const { register, handleSubmit, formState: {errors} } = useForm<DraftPatient>()
-
   const registerPatient = (data: DraftPatient) => {
-    console.log(data)
+    addPatient(data)
   }
   
   return (
--- src/components/PatientList.tsx
+++ src/components/PatientList.tsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import { usePatientStore } from "../store"
+
 
 export const PatientList = () => {
+
+  const patients = usePatientStore(state=>state.patients)
+  console.log(patients)
+
   return (
     <div>PatientList</div>
   )
